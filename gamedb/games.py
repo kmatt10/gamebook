bde029c08ddc4295ab1b72a82275345f2a985bcc
kmatt10 <kmatt10@gmail.com>
2025-10-11 08:51:44 -0500

    Update games.py
    Created QueryBuilder class to derive GameQuery and SearchQuery from to capture similar behavior

--- gamedb/games.py
+++ gamedb/games.py
@@ -3,8 +3,44 @@ from flask import (
 )
 from gamedb.db import get_db, game_to_result, GameEntry, Result
 
-class SearchQuery:
+class QueryBuilder:
     def __init__(self, args):
+        self.page = max(args.get('page', default=1, type=int),1)
+        self.page_size = min(max(args.get('page_size', default=50, type=int),1),100)
+        self.offset = (self.page - 1) * self.page_size
+        self._parameter_args = []
+        self._base_query = """SELECT id, name, released, 
+            COALESCE(platforms, "Unknown") AS platforms, 
+            COALESCE(genres, "Unknown") AS genres
+            FROM games WHERE 1=1"""
+
+    def build_query(self) -> str:
+        raise NotImplementedError
+
+    def parameter_args(self) -> tuple:
+        return tuple(self._parameter_args)
+
+class GameQuery(QueryBuilder):
+    def __init__(self, args):
+        super().__init__(args)
+        self.id = args.get('id', default = None, type=int)
+
+    def build_query(self) -> str:
+        self._parameter_args = []
+        query = self._base_query
+
+        #TODO: Add logic for selecting by id
+
+        query = query + " ORDER BY metacritic DESC LIMIT ? OFFSET ?"
+        self._parameter_args.extend((self.page_size,self.offset))
+
+        return query
+
+    
+
+class SearchQuery(QueryBuilder):
+    def __init__(self, args):
+        super().__init__(args)
         self.name = args.get('name', default=None, type=str)
         self.platform = args.get('platform', default=None, type=str)
         self.genre = args.get('genre', default=None, type=str)
@@ -15,52 +51,34 @@ class SearchQuery:
         self.metacritic_max = args.get('metacritic_max', default=None, type=int)
         self.strict = args.get('strict', default=False, type=bool)
         
-        self.page = max(args.get('page', default=1, type=int),1)
-        self.page_size = min(max(args.get('page_size', default=50, type=int),1),100)
-        self.offset = (self.page - 1) * self.page_size
         self.wild_name = f'%{self.name}%' if self.name else None
-        self._parameter_args = []
-        self._base_query = """SELECT id, name, released, 
-            COALESCE(platforms, "Unknown") AS platforms, 
-            COALESCE(genres, "Unknown") AS genres
-            FROM games WHERE 1=1
-            """
         
     def build_query(self):
         self._parameter_args = []
         query = self._base_query
 
         if self.name:
-            query = query + "AND name LIKE ?"
+            query = query + " AND name LIKE ?"
             self._parameter_args.append(self.name if self.strict else self.wild_name)
 
         #TODO: Add additional search fields
+
+        if len(self._parameter_args) == 0:
+            query = query + " AND 1=0"
 
         query = query + " ORDER BY metacritic DESC LIMIT ? OFFSET ?"
         self._parameter_args.extend((self.page_size,self.offset))
         
         return query
-    
-    def parameter_args(self):
-        return tuple(self._parameter_args)
 
 bp = Blueprint('games', __name__)
 
 @bp.route('/games')
 def games():
-    search_args = SearchQuery(request.args)
+    game_query = GameQuery(request.args)
     db = get_db()
-    query = f"""
-    SELECT id, name, released, 
-        COALESCE(platforms, "Unknown") AS platforms, 
-        COALESCE(genres, "Unknown") AS genres
-    FROM games
-    ORDER BY metacritic DESC
-    LIMIT ? OFFSET ?
-    """
-    games = db.execute(query,
-                       (search_args.page_size, 
-                        search_args.offset)).fetchall()
+    games = db.execute(game_query.build_query(),
+                       game_query.parameter_args()).fetchall()
 
     result = game_to_result(games)
     return result.to_json()
